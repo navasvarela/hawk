247ccd0a5ce83790cd4236549f182e1e83694226
namuan <nleghari@gmail.com>
2011-09-18 21:39:05 +0000

    added faye support

--- trunk/hawk-app/app.js
+++ trunk/hawk-app/app.js
@@ -1,5 +1,6 @@
 var express = require('express'),
-    mongoose = require('mongoose');
+    mongoose = require('mongoose'),
+    faye = require('faye');
 
 var app = module.exports = express.createServer();
 
@@ -24,14 +25,25 @@ app.configure('production', function(){
 });
 
 // Routes
-
 app.get('/', function(req, res){
   res.sendfile('./public/index.html');
 });
 
 require('./lib/routes/instances')(app);
 
+// start faye server
+var bayeux = new faye.NodeAdapter({mount: '/faye', timeout: 45});
+bayeux.listen(8000);
+console.log("Started faye server on port 8000");
+
 mongoose.connect('mongodb://localhost/hawk-dev');
 
 app.listen(3000);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+
+setInterval(function() {
+    console.log("New email..Sending event to all subscriber");
+    bayeux.getClient().publish('/faye', {
+        text: 'New email has arrived!' 
+    });
+}, 1000);
