b1781191f24576098a59249f957566a3239a42ab
namuan <nleghari@gmail.com>
2011-09-18 23:18:23 +0000

    used spine to create instanceroute

--- trunk/hawk-app/app.js
+++ trunk/hawk-app/app.js
@@ -1,5 +1,6 @@
 var express = require('express'),
     mongoose = require('mongoose'),
+    InstanceRoute = require('./lib/routes/instances'),
     faye = require('faye');
 
 var app = module.exports = express.createServer();
@@ -29,21 +30,20 @@ app.get('/', function(req, res){
   res.sendfile('./public/index.html');
 });
 
-require('./lib/routes/instances')(app);
-
-// start faye server
 var bayeux = new faye.NodeAdapter({mount: '/faye', timeout: 45});
-bayeux.listen(8000);
-console.log("Started faye server on port 8000");
+bayeux.attach(app);
+
+var instanceRoute = new InstanceRoute(app);
+
+InstanceRoute.bind("create", function(message) {
+    console.log("Created:" + message);
+    bayeux.getClient().publish('/faye', {
+        text: 'New email has arrived'
+    });
+});
 
 mongoose.connect('mongodb://localhost/hawk-dev');
 
 app.listen(3000);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 
-setInterval(function() {
-    console.log("New email..Sending event to all subscriber");
-    bayeux.getClient().publish('/faye', {
-        text: 'New email has arrived!' 
-    });
-}, 1000);
--- trunk/hawk-app/lib/routes/instances.js
+++ trunk/hawk-app/lib/routes/instances.js
@@ -1,63 +1,69 @@
-
 var util = require('util'),
+    Spine = require('spine'),
     Instance = require('../models/instance');
-
-var saveInstance = function(inputInstance) {
-  var instance = new Instance();
-  instance.name = inputInstance.name;
-  instance.state = inputInstance.state;
-
-  instance.save(function(err) {
-      if (err) {
-          return new Error("unable to save instance: " + instance.name + " - " + err);
-      } 
-  });
-};
-
-var getInstances = function(request, response) {
-  console.log("Hawk: getInstance");
-  Instance.find({}, function(err, instances) {
-      response.json(instances);
-  });
-};
-
-var getInstance = function(request, response) {
-  var instancename = request.params.instancename;
-  console.log("Hawk: getInstance(" + instancename + ")");
-  Instance.find({"name": instancename}, function(err, instances) {
-    if (err) {
-      console.log("Unable to find instance:" + instancename + " - " + err);
-      response.send(500);
-    } else {
-      console.dir(instances);
-      response.json(instances);
+    
+var InstanceRoute = Spine.Class.create({
+    init: function(app) {
+        app.get('/instances', this.getInstances);
+        app.get('/instances/:instancename', this.getInstance);
+        app.post('/instances', this.postInstance);
+        app.put('/instances', this.putInstance);
+    },
+    getInstances: function(request, response) {
+        console.log("Hawk: getInstances");
+        Instance.find({}, function(err, instances) {
+            response.json(instances);
+        });
+    },
+    getInstance: function(request, response) {
+        var instancename = request.params.instancename;
+        console.log("Hawk: getInstance(" + instancename + ")");
+        Instance.find({
+            "name": instancename
+        }, function(err, instances) {
+            if (err) {
+                console.log("Unable to find instance:" + instancename + " - " + err);
+                response.send(500);
+            }
+            else {
+                console.dir(instances);
+                response.json(instances);
+            }
+        });
+    },
+    poseInstance: function(request, response) {
+        console.log("Hawk: postInstance " + util.inspect(request.body));
+        var instanceName = request.body.name;
+        if (Instance.find({
+            name: instanceName
+        })) {
+            console.log("Cannot create an existing instance:" + instanceName);
+            response.send(409);
+        }
+        else {
+            InstanceRoute.saveInstance(request.body);
+            response.send('OK');
+        }
+    },
+    putInstance: function(request, response) {
+        console.log("Hawk: putInstance" + util.inspect(request.body));
+        InstanceRoute.saveInstance(request.body);
+        response.send('OK');
     }
-  });
-};
+});
 
-var postInstance = function(request, response) {
-  console.log("Hawk: postInstance " + util.inspect(request.body));
-  var instanceName = request.body.name;
+InstanceRoute.extend({
+    saveInstance: function(inputInstance) {
+        var instance = new Instance();
+        instance.name = inputInstance.name;
+        instance.state = inputInstance.state;
+        instance.save(function(err) {
+            if (err) {
+                return new Error("unable to save instance: " + instance.name + "- " + err);
+            }
+        });
+    }
+});
 
-  if (Instance.find({name: instanceName})) {
-    console.log("Cannot create an existing instance:" + instanceName);
-    response.send(409);
-  } else {
-    saveInstance(request.body);
-    response.send('OK');
-  }
-};
-
-var putInstance = function(request, response) {
-  var self = this;
-  console.log("Hawk: putInstance" + util.inspect(request.body));
-  saveInstance(request.body);
-  response.send('OK');
-};
-
-module.exports = function(app) {
-  app.get('/instances', getInstances);
-  app.get('/instances/:instancename', getInstance);
-  app.post('/instances', postInstance);
-  app.put('/instances', putInstance);
-};
+InstanceRoute.extend(Spine.Events);
+module.exports = InstanceRoute;
